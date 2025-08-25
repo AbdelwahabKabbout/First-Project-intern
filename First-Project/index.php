44cc0d3f6cdf077992b74b6b14ef4fbbad4f0f6b
AbdelwahabKabbout <abdelwahabkabbout@gmail.com>
2025-08-25 13:48:02 +0300

    adding gbook-add.php as index.php first , adding css for the page

--- First-Project/index.php
+++ First-Project/index.php
@@ -4,8 +4,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="index.css">
 </head>
 <body>
-    
+    <h1 class="Welcome-Light">Welcome to Gbook</h1>
+    <form action="post.php" method="POST">
+        <label for="Name">
+            Name:
+            <input type="text" id="Name" name="Name" required>
+        </label>
+        <label for="Email">
+            Email:
+            <input type="email" id="Email" name="Email" required>
+        </label>
+ 
+        <label for="Message" >
+  Message:
+  <input type="text" id="Message" class="Message" name="Message" required>
+</label>
+
+        
+        <button class="Submit-btn">Submit</button>
+    </form>
+
 </body>
 </html>
